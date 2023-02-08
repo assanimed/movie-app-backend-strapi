5a3fdd59f88d4ff08418e4bc25863b3ceef9a893
mohamed elmhassani <mohamed_elmhassani@live.fr>
2023-02-08 20:55:58 +0100

    JWT EXPIRE TIME EDIT

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,14 @@
 // path: ./config/plugins.js
 
 module.exports = ({ env }) => ({
+  "users-permissions": {
+    enabled: true,
+    config: {
+      jwt: {
+        expiresIn: "1m",
+      },
+    },
+  },
   upload: {
     config: {
       breakpoints: {
